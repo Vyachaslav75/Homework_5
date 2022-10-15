564b5654c9b56bd650f70b0375f6b496ffb31210
Vyachaslav75 <kslava@tut.by>
2022-10-15 13:45:37 +0300

    Add Task 38

---
+++ Task_38/Task_38.cs
@@ -0,0 +1,61 @@
+﻿//Программа задает массив вещественных чисел.
+//Находит разницу между максимальным и минимальным элементами.
+int enterNumber(string msg, string errorMsg)
+{
+    int number;
+    while (true)
+    {
+        Console.Write(msg);
+        if (int.TryParse(Console.ReadLine(), out number))
+        {
+            return number;
+        }
+        Console.WriteLine(errorMsg);
+    }
+}
+
+double[] GetArrayDouble(int size, int rank)
+{
+    double[] res = new double[size];
+    for (int i = 0; i < res.Length; i++)
+    {
+        //res[i] = new Random().Next(minValue, maxValue + 1);
+        res[i] = GetDoubleRandom(rank);
+    }
+
+    return res;
+}
+
+double GetDoubleRandom(int usRank)
+{
+    double myDouble = new Random().NextDouble();
+    int rank = new Random().Next(0, usRank + 1);
+    myDouble = myDouble * Math.Pow(10, rank);
+    return myDouble;
+}
+
+double MinElt(double[] array)
+{
+    double minEl = array[0];
+    foreach (double item in array)
+    {
+        if (item < minEl) minEl = item;
+    }
+    return minEl;
+}
+double MaxElt(double[] array)
+{
+    double maxEl = array[0];
+    foreach (double item in array)
+    {
+        if (item > maxEl) maxEl = item;
+    }
+    return maxEl;
+}
+
+Console.Clear();
+
+int Size = enterNumber("Введите количество элементов массива:  ", "Введите целое число");
+int myRank = enterNumber("Введите максимальный порядок числа:  ", "Введите целое число");
+double[] array = GetArrayDouble(Size, myRank);
+Console.WriteLine($"[{String.Join(", ", array)}] -> {MaxElt(array) - MinElt(array)}");
